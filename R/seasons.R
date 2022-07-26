e7cf985933b14ef829d0236206757733d5b392b2
mjoygroves <mjgeist@ncsu.edu>
2022-07-26 16:09:47 -0400

    feat: accumulate seasons

--- R/seasons.R
+++ R/seasons.R
@@ -12,11 +12,11 @@
 #' @noRd
 #'
 seasons_intermediate <- function(data_teams_matches, fn_points_per_win) {
-  
+
   # 2.4.1 Misc. (as_function())
   #
   # 1. accept purrr-style anonymus functions
-  
+
   result <-
     data_teams_matches |>
     # across() lets you use tidy-select semantics inside data-masking functions
@@ -26,18 +26,17 @@ seasons_intermediate <- function(data_teams_matches, fn_points_per_win) {
     ) |>
     dplyr::arrange(.data$date, .by_group = TRUE) |>
     dplyr::transmute(
-      date = .data$date,
+      date    = .data$date,
       matches = TRUE,
-      wins = .data$goals_for > .data$goals_against,
-      draws = .data$goals_for == .data$goals_against,
-      losses = .data$goals_for < .data$goals_against,
-      points =
-        fn_points_per_win(.data$country, .data$season) * .data$wins +
-        .data$draws,
-      goals_for = .data$goals_for,
+      wins    = .data$goals_for > .data$goals_against,
+      draws   = .data$goals_for == .data$goals_against,
+      losses  = .data$goals_for < .data$goals_against,
+      points  = fn_points_per_win(
+        .data$country, .data$season) * .data$wins + .data$draws,
+      goals_for     = .data$goals_for,
       goals_against = .data$goals_against
     )
-  
+
   result
 }
 
@@ -53,7 +52,7 @@ seasons_intermediate <- function(data_teams_matches, fn_points_per_win) {
 #' @param fn_points_per_win `function` with vectorized arguments `country`,
 #'   `season`, that returns a integer indicating points-per-win.
 #'   A default function is provided, [uss_points_per_win()], which includes
-#'   the countries in [uss_countries()]. 
+#'   the countries in [uss_countries()].
 #'
 #' @return [tibble][tibble::tibble-package] with columns
 #'   `country`, `tier`, `season`, `team`, `date`, `matches`, `wins`,
@@ -61,25 +60,28 @@ seasons_intermediate <- function(data_teams_matches, fn_points_per_win) {
 #' @examples
 #' italy <- uss_get_matches("italy") |> uss_make_teams_matches()
 #' uss_make_seasons_cumulative(italy)
+#' italy <- uss_get_matches("italy") |> uss_make_teams_matches()
+#' uss_make_seasons_cumulative(italy) |> glimpse()
+#' uss_make_seasons_final(italy) |> glimpse()
 #' @export
 #'
 uss_make_seasons_cumulative <- function(data_teams_matches,
                                         fn_points_per_win = uss_points_per_win) {
-  
+
   validate_data_frame(data_teams_matches)
   validate_cols(data_teams_matches, cols_teams_matches())
-  
+
   result <-
     data_teams_matches |>
     seasons_intermediate(fn_points_per_win) |>
     dplyr::mutate(
       dplyr::across(cols_seasons_accumulate(), cumsum)
-    ) 
-  
+    )
+
   result
 }
 
-# 2.3.6 tidy eval (exercise) 
+# 2.3.6 tidy eval (exercise)
 #
 # Create a new function: `uss_make_seasons_final()`:
 #
@@ -90,16 +92,35 @@ uss_make_seasons_cumulative <- function(data_teams_matches,
 # 4. You can use the same validation (then test the validation by uncommenting).
 # 5. Instead of `dplyr::mutate()`, you can use `dplyr::summarise()`.
 # 6  You will want to `sum` quantities, rather than `cumsum`.
-# 7. You will have to summarise the `date` differently than 
+# 7. You will have to summarise the `date` differently than
 #    `cols_seasons_accumulate()`.
 # 8. Use the `arrange_final()` function to arrange the results into "normal"
 #    standings.
 # 9. Test the function by uncommenting the tests.
 
+#' @rdname uss_make_seasons_cumulative
+#' @export
+#'
+uss_make_seasons_final <- function(data_teams_matches,
+                                   fn_points_per_win = uss_points_per_win) {
+  validate_data_frame(data_teams_matches)
+  validate_cols(data_teams_matches, cols_teams_matches())
+
+  result <-
+    data_teams_matches |>
+    seasons_intermediate(fn_points_per_win) |>
+    dplyr::summarise(
+      date = max(.data$date),
+      dplyr::across(cols_seasons_accumulate(), sum)
+    ) |>
+    arrange_final()
+
+  result
+}
 
 # internal function to arrange season-final standings
 arrange_final <- function(data) {
-  
+
     result <-
       data |>
       dplyr::group_by(
